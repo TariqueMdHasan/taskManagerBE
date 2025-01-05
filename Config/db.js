9ce11544c690e3262b6a52583f124345915040d1
Md Tarique Hasan <TariqueMdHasan@gmail.com>
2025-01-06 02:59:12 +0530

    ,

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 30000, // Optional: Increase timeout to 30 seconds
+        })
         .then(()=>console.log('server successfully conected to database'))
         .catch((error)=> console.log('not connected to database', error))
     }catch(error){
