0c9d5a1e8d5247599f3f4434efdc12567028afde
Md Tarique Hasan <TariqueMdHasan@gmail.com>
2025-01-06 02:48:02 +0530

    something added

--- Config/db.js
+++ Config/db.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose');
 
 const connectDB = async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
         .then(()=>console.log('server successfully conected to database'))
         .catch((error)=> console.log('not connected to database', error))
     }catch(error){
         console.log('something went wrong in db.js', error)
+
     }
 }
 
